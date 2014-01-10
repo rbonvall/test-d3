0561a87a6491ddd61813ab62145c72c59ee13866
Roberto Bonvallet <roberto@nimbic.com>
2014-01-10 16:49:00 -0300

    Add functional.js

--- config.js
+++ config.js
@@ -1,11 +1,15 @@
 require.config({
     paths: {
         d3: 'lib/d3/d3.v3.min',
-        lodash: 'lib/lodash/lodash.min'
+        lodash: 'lib/lodash/lodash.min',
+        functional: 'lib/functional-1.0.2/functional.min'
     },
     shim: {
         d3: {
             exports: 'd3'
+        },
+        functional: {
+            exports: 'Functional'
         }
     }
 });
