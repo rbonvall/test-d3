08b7fae016aedd6d26de0bf705a08af10f604c7f
Roberto Bonvallet <roberto@nimbic.com>
2014-01-10 16:14:43 -0300

    Make fancy graph

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ require(['d3', 'lodash'], function (d3, _) {
 
     var square = function (x) { return x * x; };
     var exclamate = function (x) { return '¡' + x + '!'; };
+    var redish = function (x) { return 'hsl(0, x%, 35%'.replace('x', x); };
+
 
     d3.select('#chart')
         .selectAll('p')
-        .data(_.range(10).map(square))
+        .data(_.range(1, 10).map(square))
         .enter()
             .append('p')
-            .text(exclamate);
-
+            .text(exclamate)
+            .style('width', function (x) { return 30 + 5 * x + 'px'; } )
+            .style('color', redish)
+        ;
 });
 });
 
