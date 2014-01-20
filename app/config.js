ea065c32aae5844b05a4786bc5c071b4f125880b
Roberto Bonvallet <roberto@nimbic.com>
2014-01-20 10:30:25 -0300

    Install nvd3

--- app/config.js
+++ app/config.js
@@ -1,12 +1,17 @@
 require.config({
     paths: {
         d3: 'lib/d3/d3.v3.min',
+        nv: 'lib/nvd3/nv.d3.min',
         lodash: 'lib/lodash/lodash.min',
         functional: 'lib/functional-1.0.2/functional.min'
     },
     shim: {
         d3: {
             exports: 'd3'
+        },
+        nv: {
+            exports: 'nv',
+            deps: ['d3']
         },
         functional: {
             exports: 'Functional'
