cce8ce74b43402fc244ae3110aab461a93dfd435
Roberto Bonvallet <roberto@nimbic.com>
2014-01-22 18:22:40 -0300

    Example 7: multiline chart
    With a little help of the master himself: http://stackoverflow.com/a/8730433/13169

---
+++ app/app7.js
@@ -0,0 +1,83 @@
+require(['config'], function () {
+require(['d3', 'lodash', 'functional'], function (d3, _, F) {
+    'use strict';
+
+    var λ = F.lambda;
+
+    var width = 500, height = 300, padding = 40;
+
+    var dataLength = 10;
+    var dataScale = d3.scale.linear()
+        .domain([0, 1])
+        .range([20, 40]);
+    var generate = F.sequence(Math.random, dataScale, Math.floor);
+    var dataset = [
+        d3.range(dataLength).map(generate),
+        d3.range(dataLength).map(generate).map(λ('+25')),
+        d3.range(dataLength).map(generate).map(λ('+50')),
+    ];
+
+    // Coordinate scaling functions
+    var x = d3.scale.linear()
+        .domain([0, dataLength])
+        .range([padding, width - padding]);
+    var y = d3.scale.linear()
+        .domain([0, 100])
+        .range([height - padding, padding]);
+
+    // Define axes
+    var xAxis = d3.svg.axis().scale(x).ticks(dataLength);
+    var yAxis = d3.svg.axis().scale(y).ticks(5).orient('left');
+
+    var svg = d3.select('#chart').append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    // Draw axes
+    svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(0, Y)'.replace('Y', height - padding))
+        .call(xAxis);
+    svg.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(X, 0)'.replace('X', padding))
+        .call(yAxis);
+
+    // Define line function
+    var line = d3.svg.line().x(F.flip(x)).y(y);
+
+    // Draw lines
+    svg.selectAll('path.dataLine')
+        .data(dataset)
+        .enter()
+            .append('path')
+            .attr('class', 'dataLine')
+            .attr('d', line)
+            .attr('stroke', d3.scale.category20());
+
+    // Put labels
+    svg.selectAll('g.dataLabels')
+        .data(dataset)
+        .enter()
+            .append('g')
+            .attr('class', 'dataLabels')
+
+        .selectAll('text.dataLabel')
+        .data(F.id)
+        .enter()
+            .append('text')
+            .attr('class', 'dataLabel')
+            .text(Math.floor)
+            .attr('x', F.flip(x))
+            .attr('y', F.sequence(λ('+5'), y));
+
+    // Show raw data on the document
+    d3.select('#data')
+        .html(JSON.stringify(dataset)
+                .replace(/,/g, ',\n')
+                .replace(/\],/g, '],<br>'));
+
+});
+});
+
+
