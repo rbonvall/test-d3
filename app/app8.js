da73d523e940ab9f1f5d1f32e7216ceca3beb99e
Roberto Bonvallet <roberto@nimbic.com>
2014-01-24 18:30:37 -0300

    (Non-working) reusable chart

--- app/app8.js
+++ app/app8.js
@@ -1,48 +1,21 @@
 require(['config'], function () {
-require(['d3'], function (d3) {
+require(['d3', 'simple'], function (d3, simpleChart) {
     'use strict';
-
-    function addGetterSetter(object, config, property) {
-        var getterSetter = function (value) {
-            if (!arguments.length) {
-                return config[property];
-            }
-            config[property] = value;
-            return object;
-        }
-        object[property] = getterSetter;
-    }
-
-    function simpleChart() {
-        var config = {
-            width: 100,
-            height: 80,
-            padding: 5,
-
-            xValue: function (d, i) { return i; },
-            yValue: function (d, i) { return d; },
-            domain: [0, 1]
-        };
-
-        function my(selection) {
-            selection.each(function (data) {
-
-            });
-        };
-
-        d3.keys(config).forEach(function (prop) {
-            addGetterSetter(my, config, prop);
-        });
-
-        return my;
-    }
 
     var c = simpleChart()
         .width(50)
         .height(100);
 
-    d3.select('#chart')
-        .data([80, 100, 30, 70, 25])
+    // d3.select('#chartA')
+    //     .data([80, 100, 30, 70, 25])
+    //     .call(c);
+
+    d3.select('#chartB')
+        .data([500, 200, 300, 150, 200])
+        .call(c);
+
+    d3.select('#chartC')
+        .data([[70, 70, 70, 90, 30], [30, 40, 40, 60, 70]])
         .call(c);
 
 });
---
+++ app/simple.js
@@ -0,0 +1,97 @@
+define(['d3', 'lodash', 'functional'], function (d3, _, F) {
+    'use strict';
+
+    var λ = F.lambda;
+
+    function addGetterSetter(object, config, property) {
+        object[property] = function (value) {
+            if (!arguments.length) {
+                return config[property];
+            }
+            config[property] = value;
+            return object;
+        }
+    }
+
+    function simpleChart() {
+        var conf = {
+            width: 100,
+            height: 80,
+            padding: 5,
+            stroke: d3.scale.category20()
+        };
+
+        function createChart(data) {
+            console.log('XXX', JSON.stringify({data: data}));
+            if (!_.isArray(data[0])) {
+                data = [data];
+                console.log('XXX', JSON.stringify({converted: data}));
+            }
+
+            var flattened = _.flatten(data);
+            var min = Math.min(flattened);
+            var max = Math.max(flattened);
+            var nrPlots = data.length;
+            var n = Math.max(_.pluck(data, 'length'));
+
+            var x = d3.scale.linear()
+                .domain([0, n - 1])
+                .range([conf.padding, conf.width - conf.padding]);
+            var y = d3.scale.linear()
+                .domain([min, max])
+                .range([conf.height - conf.padding, conf.width]);
+
+            var line = d3.svg.line()
+                .x(F.flip(x))
+                .y(y);
+
+            var svg = d3.select(this)
+                .append('svg')
+                .attr('width', conf.width)
+                .attr('height', conf.height);
+
+            // Draw lines
+            svg.selectAll('path.dataLine')
+                .data(data)
+                .enter()
+                    .append('path')
+                    .classed('dataLine', true)
+                    .attr('d', line)
+                    .attr('stroke', conf.stroke);
+
+            // Put labels
+            svg.selectAll('g.dataLabels')
+                .data(data)
+                .enter()
+                    .append('g')
+                    .classed('dataLine', true)
+
+                .selectAll('text.dataLabel')
+                .data(F.id)
+                .enter()
+                    .append('text')
+                    .classed('dataLabel', true)
+                    .text(Math.floor)
+                    .attr('x', F.flip(x))
+                    .attr('y', F.sequence(λ('+5'), y));
+
+        }
+
+
+        function my(selection) {
+            selection.each(createChart);
+        }
+
+        d3.keys(conf).forEach(function (prop) {
+            addGetterSetter(my, conf, prop);
+        });
+
+        return my;
+    }
+
+    return simpleChart;
+
+});
+
+
+
