6bf16c71d919b2f7abee282c61bfc28d9528fde8
Roberto Bonvallet <roberto@nimbic.com>
2014-01-27 13:07:32 -0300

    Fancier version of the document

--- app/app8.js
+++ app/app8.js
@@ -3,19 +3,27 @@ require(['d3', 'simple'], function (d3, simpleChart) {
     'use strict';
 
     var c = simpleChart()
-        .width(50)
-        .height(100);
+        .width(500)
+        .height(200)
+        .padding(40);
 
-    // d3.select('#chartA')
-    //     .data([80, 100, 30, 70, 25])
-    //     .call(c);
+    d3.select('#chartA')
+        .datum([80, 100, 30, 70, 25])
+        .call(c);
 
     d3.select('#chartB')
-        .datum([500, 200, 300, 150, 200])
+        .datum([
+            [500, 200, 300, 150, 200],
+            [400, 150, 200, 100, 300]
+        ])
         .call(c);
 
     d3.select('#chartC')
-        .datum([[70, 70, 70, 90, 30], [30, 40, 40, 60, 70]])
+        .datum([
+            [70, 65, 70, 70, 90, 30],
+            [30, 40, 40, 60, 70],
+            [20, 50, 100]
+        ])
         .call(c);
 
 });
